83617d8b0da7f9d5c24b25f3f930b8b768e421a9
JpCardenasG <jpcardenasgonzalez.com>
2024-08-01 08:18:27 -0500

    feat: :tada: Begin of the Grab and Go Project
    @trainingLeader

---
+++ src/main/java/com/grabandgo/grabandgo_backend/GrabandgoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grabandgo.grabandgo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabandgoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrabandgoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grabandgo/grabandgo_backend/GrabandgoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grabandgo.grabandgo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabandgoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
